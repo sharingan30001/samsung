1dba53be7939880e73917be36c2d7c5d82bf35a0
sharingan30001 <79019288+sharingan30001@users.noreply.github.com>
2024-05-24 16:42:48 +0500

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.project_samsung;
 
+import static android.app.ProgressDialog.show;
+
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 import com.example.project_samsung.color;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,38 +25,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        EditText editText = (EditText) findViewById(R.id.editText);
+        Button button = (Button) findViewById(R.id.button);
         Button button1, button2;
         final RelativeLayout relativeLayout;
-
         // set button 1 with its id
-        button1 = findViewById(R.id.btVar1);
 
         // set button 2 with its id
         // set relative layout with its id
         relativeLayout = findViewById(R.id.rlVar1);
-
-        // onClick function for button 1
-        final int[] clickcount = {0};
-        button1.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                // set the color to relative layout
-                Random color = new Random();
-                relativeLayout.setBackgroundColor(Color.argb(255, color.nextInt(255), color.nextInt(255), color.nextInt(255)));
-                clickcount[0]++;
-                if(clickcount[0] == 1)
-                {
-                    //first time c
-                    //licked to do this
-                    Toast.makeText(getApplicationContext(),"Button clicked first time!", Toast.LENGTH_LONG).show();
+            public void onClick(View v) {
+                String text = editText.getText().toString();
+                String[] a = text.split(" ");
+                Integer[] ints = new Integer[3];
+                for (int i = 0; i < a.length; ++i) {
+                    ints[i] = Integer.parseInt(a[i]);
                 }
-                else
-                {
-                    //check how many times clicked and so on
-                    Toast.makeText(getApplicationContext(),"Button clicked count is"+ clickcount[0], Toast.LENGTH_LONG).show();
-                }
-
+                color c = new color();
+                String ans = c.getColorNameFromRgb(ints[0], ints[1], ints[2]);
+                relativeLayout.setBackgroundColor(Color.argb(255, ints[0], ints[1], ints[2]));
             }
         });
     }
